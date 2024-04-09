4f9ffdd9a1166245f7c7fb8dd00431f0cb793f2a
victor.damasceno <victordamascenos2015@gmail.com>
2024-04-09 19:10:12 -0300

    Adiciona modelo de usuário (User model)

---
+++ back-end/src/main/java/com/example/todolist/model/User.java
@@ -0,0 +1,22 @@
+package com.example.todolist.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
