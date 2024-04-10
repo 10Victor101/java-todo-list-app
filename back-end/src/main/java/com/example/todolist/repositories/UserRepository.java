f41efcc403e74125e56721d490b689637b7cb8d7
victor.damasceno <victordamascenos2015@gmail.com>
2024-04-09 21:42:08 -0300

    Implementação do JWT para autenticação de usuários

--- back-end/src/main/java/com/example/todolist/repositories/UserRepository.java
+++ back-end/src/main/java/com/example/todolist/repositories/UserRepository.java
@@ -4,5 +4,9 @@ package com.example.todolist.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.todolist.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String username);
+    Optional<User> findUserById(Integer id);
 }
